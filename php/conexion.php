0de3a5373a20464c72d8aed2d81dca87fc60a6e8
ByronCriollo <38530046+ByronCriollo@users.noreply.github.com>
2018-05-24 07:49:25 -0500

    bdd

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
-$host="localhost";
-$user="root";
-$password="";
+$host="baseserv.mysql.database.azure.com";
+$user="myapp@baseserv";
+$password="2092Acer";
 $db="myapp";
 $con = new mysqli($host,$user,$password,$db);
 
